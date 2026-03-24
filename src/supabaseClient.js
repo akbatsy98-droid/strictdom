ba825775f484219567e9cfb50471b2ab76ba9bd0
akbatsy98-droid <akbatsy98@gmail.com>
2026-03-24 12:26:55 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://tiwwjcfwqckzeevctya.supabase.co'
-const supabaseKey = 'sb_publishable_qRhtXHJdu3TafGr37npumQ_UHEBbA6f'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpd3dqY2Z3cWNremVldmNxdHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMTkxMjEsImV4cCI6MjA4OTg5NTEyMX0.SiYbGg4i-OvlAhofHVi-smuU6fzQVQQMFN0y8rh-_d8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
